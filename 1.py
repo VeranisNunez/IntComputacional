b22a91564cc1247c4d07df08a11ce1033ef57fb2
Usuario Invitado <invitado@cuc.edu.co>
2022-03-02 19:32:40 -0500

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+"""
+Una Institución educativa ha recibido una donación especial que será
+repartida entre las carreras de Telecomunicaciones, Sistemas,
+Administración y Contabilidad de la siguiente forma:
+a. Telecomunicaciones 10% del valor dado a sistemas
+b. Sistemas: 25% del valor dado a Administración
+c. Administración: 35% del valor de la donación
+d. Contabilidad: lo que resta de la donación
+"""
